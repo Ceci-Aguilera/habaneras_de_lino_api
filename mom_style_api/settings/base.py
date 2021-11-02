dc3a3d71567cb88c686bc01235f64ef37a7ebaaa
Ceci-Aguilera <ceci.aguilera@zohomail.com>
2021-11-01 23:59:41 -0400

    wrap around whitenoise

--- mom_style_api/settings/base.py
+++ mom_style_api/settings/base.py
@@ -127,7 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(ROOT_BASE_DIR,'staticfiles')]
 STATIC_ROOT = os.path.join(ROOT_BASE_DIR,'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(ROOT_BASE_DIR, 'media')
